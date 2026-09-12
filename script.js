d6441700f2aa7fc679b561582ab79c7b1ef7a37e
13en3volen7 <64196796+13en3volen7@users.noreply.github.com>
2026-09-12 17:10:49 +0700

    Simplify makeGrid logic

--- script.js
+++ script.js
@@ -1,26 +1,22 @@
-function makeSquare(fromDiv, size = 16) {
+function createSquareTemplate(fromDiv, size = 16) {
     const squareDiv = document.createElement("div");
     const squareSize = fromDiv.clientWidth / size;
     squareDiv.style.width = `${squareSize}px`;
     squareDiv.style.height = `${squareSize}px`;
-    squareDiv.classList.add("square");
-    fromDiv.appendChild(squareDiv);
-}
-
-function makeGridRow(fromDiv, rowID, size = 16) {
-    const gridRowDiv = document.createElement("div");
-    gridRowDiv.classList.add("grid-row", `id${rowID}`);
-    fromDiv.appendChild(gridRowDiv);
-    for (let column = 1; column <= size; column++) {
-        makeSquare(gridRowDiv, size);
-    }
+    return squareDiv;
 }
 
 function makeGrid(size = 16) {
     const containerDiv = document.querySelector("#container");
+    const squareDiv = createSquareTemplate(containerDiv, size);
     for (let row = 1; row <= size; row++) {
-        makeGridRow(containerDiv, row, size);
+        for (let column = 1; column <= size; column++) {
+            const squareDivClone = squareDiv.cloneNode();
+            squareDivClone.classList.add("square", `r${row}`, `c${column}`);
+            containerDiv.appendChild(squareDivClone);
+        }
     }
 }
+
 
 makeGrid(100);
