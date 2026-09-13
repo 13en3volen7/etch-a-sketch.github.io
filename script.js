64551c5f05bb29c5fb559c7c798351c4b0b7fd55
13en3volen7 <64196796+13en3volen7@users.noreply.github.com>
2026-09-13 11:00:54 +0700

    Add rainbow hovering effect

--- script.js
+++ script.js
@@ -21,6 +21,30 @@ function initialize(gridSize = 16) {
     }
 
     makeGrid();
+    containerDiv.addEventListener("mouseover", handleMouseOver);
 }
 
-initialize(100);
+function getRandomIntInclusive(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+}
+
+function getRandomRGBValue() {
+    const result = [];
+    for (let i = 0; i < 3; i++) {
+        result.push(getRandomIntInclusive(0, 255));
+    }
+    return result;
+}
+
+function handleMouseOver(event) {
+    const target = event.target;
+    if (!target.classList.contains("square")) {
+        return;
+    }
+    const [r, g, b] = getRandomRGBValue();
+    target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+}
+
+initialize();
