c8094a389c51f1ea52df33a1cb0db035115c2196
13en3volen7 <64196796+13en3volen7@users.noreply.github.com>
2026-09-13 09:49:25 +0700

    Refactor initialization logic for grid

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
-function createSquareTemplate(fromDiv, size = 16) {
-    const squareDiv = document.createElement("div");
-    const squareSize = fromDiv.clientWidth / size;
-    squareDiv.style.width = `${squareSize}px`;
-    squareDiv.style.height = `${squareSize}px`;
-    return squareDiv;
-}
-
-function makeGrid(size = 16) {
+function initialize(gridSize = 16) {
     const containerDiv = document.querySelector("#container");
-    const squareDiv = createSquareTemplate(containerDiv, size);
-    for (let row = 1; row <= size; row++) {
-        for (let column = 1; column <= size; column++) {
-            const squareDivClone = squareDiv.cloneNode();
-            squareDivClone.classList.add("square", `r${row}`, `c${column}`);
-            containerDiv.appendChild(squareDivClone);
+
+    function createSquareTemplate() {
+        const squareDiv = document.createElement("div");
+        const squareSize = containerDiv.clientWidth / gridSize;
+        squareDiv.style.width = `${squareSize}px`;
+        squareDiv.style.height = `${squareSize}px`;
+        return squareDiv;
+    }
+
+    function makeGrid() {
+        const squareDiv = createSquareTemplate();
+        for (let row = 1; row <= gridSize; row++) {
+            for (let column = 1; column <= gridSize; column++) {
+                const squareDivClone = squareDiv.cloneNode();
+                squareDivClone.classList.add("square", `r${row}`, `c${column}`);
+                containerDiv.appendChild(squareDivClone);
+            }
         }
     }
+
+    makeGrid();
 }
 
-
-makeGrid(100);
+initialize(100);
