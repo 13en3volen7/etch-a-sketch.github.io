7a7dbf6b364c4f60b8d920e98cb52f9fc3b199cf
13en3volen7 <64196796+13en3volen7@users.noreply.github.com>
2026-09-13 21:38:51 +0700

    Add support for resizing grid

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function initialize(gridSize = 16) {
     const containerDiv = document.querySelector("#container");
-
+    const gridDiv = containerDiv.querySelector(".grid");
+    const optionsDiv = containerDiv.querySelector(".options");
+    
     function createCellTemplate() {
         const cellDiv = document.createElement("div");
-        const cellSize = containerDiv.clientWidth / gridSize;
+        const cellSize = gridDiv.clientWidth / gridSize;
         cellDiv.style.width = `${cellSize}px`;
         cellDiv.style.height = `${cellSize}px`;
         return cellDiv;
@@ -11,17 +13,20 @@ function initialize(gridSize = 16) {
 
     function makeGrid() {
         const cellDiv = createCellTemplate();
+        const cells = [];
         for (let row = 1; row <= gridSize; row++) {
             for (let column = 1; column <= gridSize; column++) {
                 const cellDivClone = cellDiv.cloneNode();
                 cellDivClone.classList.add("cell", `r${row}`, `c${column}`);
-                containerDiv.appendChild(cellDivClone);
+                cells.push(cellDivClone);
             }
         }
+        gridDiv.replaceChildren(...cells);
     }
 
     makeGrid();
-    containerDiv.addEventListener("mouseover", handleMouseOver);
+    gridDiv.addEventListener("mouseover", handleMouseOver);
+    optionsDiv.addEventListener("click", handleMouseClick);
 }
 
 function getRandomIntInclusive(min, max) {
@@ -45,6 +50,24 @@ function handleMouseOver(event) {
     }
     const [r, g, b] = getRandomRGBValue();
     target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+}
+
+function handleMouseClick(event) {
+    const target = event.target;
+    switch (target.className) {
+        case "resize":
+            const newSize = +prompt("Enter new grid size: ");
+            if (isNaN(newSize)) {
+                alert("Please enter a valid number");
+            } else if (newSize <= 0) {
+                alert("Grid size must be greater than 0")
+            } else if (newSize > 100) {
+                alert("Grid size must be lesser or equal than 100");
+            } else {
+                initialize(newSize);
+            }
+            return;
+    }
 }
 
 initialize();
